19fcd6337e45b63cf957cd0d3e8cccaa4563a773
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-08-28 20:34:26 +0000

    snek stuff

--- client.js
+++ client.js
@@ -1,10 +1,30 @@
-const connect = function() {
-    const conn = net.createConnection({ 
-      host: '192.168.88.151',
-      port: 50541
-    });
-    // interpret incoming data as text
-    conn.setEncoding('utf8'); 
-  
-    return conn;
-  }
+const net = require('net');
+
+const x = {
+    connect: () => {
+        const conn = net.createConnection({ 
+            host: '192.168.88.151',
+            port: 50541
+          });
+          // interpret incoming data as text
+          conn.setEncoding('utf8');
+
+          conn.on('data', (data) => {
+            console.log('Successfully connected to game server');
+          });
+
+          conn.on('connect', () => {
+            conn.write('Name: Tom');
+          });
+
+        //   conn.on('connect', () => {
+        //     conn.write('Move: up');
+        //   });
+        
+          return conn;
+        }  
+    }
+
+const { connect }  = x;
+
+module.exports = { connect }
