09f25b97f3aa5aca0955e77b672429bb1fd3894b
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-08-28 19:53:49 +0000

    snek stuff

---
+++ client.js
@@ -0,0 +1,10 @@
+const connect = function() {
+    const conn = net.createConnection({ 
+      host: '192.168.88.151',
+      port: 50541
+    });
+    // interpret incoming data as text
+    conn.setEncoding('utf8'); 
+  
+    return conn;
+  }
